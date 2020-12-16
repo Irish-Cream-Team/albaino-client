d7b20c4c8ff553361ba89bb73460330ab952ea6a
shavitAvizemer <shavit1111@gmail.com>
2020-12-16 14:21:37 +0200

    style: added some style changes.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <v-app>
-    <v-app-bar absolute color="#43a047" dark>
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
-        ></v-img>
-      </template>
+    <v-hover>
+      <v-card-title
+      slot-scope="{ hover }"
+      :class="`${hover? 'class1': 'class2'}`">
+      
+        <v-app-bar absolute color="#43a047" dark>
+          <template v-slot:img="{ props }">
+            <v-img
+              v-bind="props"
+              gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)">
+            </v-img>
+          </template>
 
-      <v-toolbar-title>YESODOT</v-toolbar-title>
-    </v-app-bar>
-    <v-container style="height: 80px;"></v-container>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-toolbar-title>YESODOT</v-toolbar-title>
+
+
+        </v-app-bar>
+      </v-card-title>
+    </v-hover>
+    
+    <v-container style="height: 800px;"></v-container>
+
     <v-main>
       <router-view></router-view>
     </v-main>
+    
   </v-app>
 </template>
 
 
-<style>
-  html { overflow-y: auto }
-</style>
 
 
 <script>
