6f0da29250e7b984703b01b1d24b5a09cb3fb205
shavitAvizemer <shavit1111@gmail.com>
2020-12-16 12:01:53 +0200

    first code.

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar absolute color="#43a047" dark>
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+
+      <v-toolbar-title>YESODOT</v-toolbar-title>
+    </v-app-bar>
+    <v-container style="height: 80px;"></v-container>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+
+<style>
+  html { overflow-y: auto }
+</style>
+
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
