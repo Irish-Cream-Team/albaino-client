20f27b2ca37be45a83c48ab3094b389fe775564d
shavitAvizemer <shavit1111@gmail.com>
2021-01-05 17:08:09 +0200

    style: added new changes.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,32 @@
 <template>
   <div class="home">
     <h1 class="display-1">Hello Shavit, welcome back</h1>
-    <v-card class="mx-auto" color="#43a047" dark max-width="400">
-      <v-img gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)">
-        <v-card-title class="justify-center">
+    <v-card color="#43a047" dark max-width="400">
+      <v-img gradient="to top right, rgba(55,236,186,1), rgba(25,32,72,.7)">
+        <v-card-title class="justify-center" max-width="400">
           <span class="headline font-weight-medium">My Projects</span>
         </v-card-title>
+        <div id="textOutline" class="d-flex flex-column align-start">
+          <v-btn
+            text
+            x-large
+            class="text-capitalize headline d-flex justify-space-between font-weight-bold"
+            min-width="100%"
+          >
+            Aman Board
+            <div>Version 1</div></v-btn
+          >
 
-        <v-card-text class="headline font-weight-small">
-        <v-hover>
-          Aman Board
-        </v-hover>
-        </v-card-text>
-
-        <v-card-text class="headline font-weight-small">
-          Aman Board
-        </v-card-text>
-
-        <v-card-text class="headline font-weight-small">
-          Aman Board
-        </v-card-text>
+          <v-btn
+            text
+            x-large
+            class="text-capitalize headline d-flex justify-space-between font-weight-bold"
+            min-width="100%"
+          >
+            Aman Board
+            <div>Version 2.2</div></v-btn
+          >
+        </div>
       </v-img>
     </v-card>
   </div>
@@ -34,3 +41,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+#textOutline {
+  -webkit-text-stroke: 0.4px black;
+}
+</style>
